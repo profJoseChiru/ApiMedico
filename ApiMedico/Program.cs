9ec7825f2df850207cbe7491a9073990f81bd23b
JChiru <prof.chirujose@gmail.com>
2024-10-28 08:27:47 -0500

    Kestrel se configura para permitir el acceso tanto desde localhost como desde otras direcciones IP en la red. Además, se configura CORS para permitir que aplicaciones web de diferentes orígenes se comuniquen de manera segura.

--- ApiMedico/Program.cs
+++ ApiMedico/Program.cs
@@ -3,9 +3,27 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configurar Kestrel para que escuche en todas las IPs
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.ListenAnyIP(5226); // Cambia el puerto si es necesario
+});
+
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Configurar CORS
+//Cross-Origin Resource Sharing (CORS)=>Compartición de Recursos entre Orígenes 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
